90b0de97eedb749af9a0a3cddea06cbfb609b087
Saar Schnieders <saar.schnieders@gmail.com>
2023-03-20 12:49:24 +0100

    week 11

--- visualizations.r
+++ visualizations.r
@@ -44,19 +44,63 @@ just_tiktok_billboard$type <- factor(x)
 ###########################################################################
 ###########################################################################
 
-# histogram of keys
-keys <- just_tiktok_billboard %>%
-  group_by(key_name, type, year) %>%
-  summarize(count = n()) %>%
-  ungroup(key_name) %>%
-  mutate(frequency = count / sum(count))
+# average tiktok songs
+tempi <- just_tiktok %>%
+  filter(tempo > 120, tempo < 130,
+         energy > 0.60, energy < 0.75,
+         track.duration_s > 160, track.duration_s < 210,
+         danceability > 0.68, danceability < 0.80,
+         valence > 0.40, valence < 0.65
+         )
 
-keys <- keys %>%
-  ggplot(aes(x = key_name, frequency, fill = type), stat = "count") +
-  geom_col(position = "dodge") +
-  labs(x = "Frequency Ratio", 
-       y = "Count",
-       title = "Distribution Across Keys by year by type") +
-  facet_wrap(~ year)
+cannibal_dtp <-
+  get_tidy_audio_analysis("3JTMWdhcJPiegDSe7SvZS3") |>
+  compmus_align(sections, segments) |> # Change `bars`
+  select(sections) |> # in all three
+  unnest(sections) |> # of these lines.
+  mutate(
+    pitches =
+      map(segments,
+          compmus_summarise, pitches,
+          method = "rms", norm = "euclidean" # Change summary & norm.
+      )
+  ) |>
+  mutate(
+    timbre =
+      map(segments,
+          compmus_summarise, timbre,
+          method = "rms", norm = "euclidean" # Change summary & norm.
+      )
+  )
 
-ggplotly(keys)
+cannibal_dtp <- cannibal_dtp |>
+  compmus_self_similarity(pitches, "cosine") |>
+  ggplot(
+    aes(
+      x = xstart + xduration / 2,
+      width = xduration,
+      y = ystart + yduration / 2,
+      height = yduration,
+      fill = d
+    )
+  ) +
+  geom_tile() +
+  coord_equal() +
+  labs(x = "cannibal", y = "cannibal", fill = "distance") +
+  theme_minimal() +
+  scale_fill_viridis_c()
+
+cannibal_tempo <- get_tidy_audio_analysis("3JTMWdhcJPiegDSe7SvZS3")
+
+cannibal_tempo <- cannibal_tempo |>
+  tempogram(window_size = 8, hop_size = 1, cyclic = TRUE) |>
+  ggplot(aes(x = time, y = bpm, fill = power)) +
+  geom_raster() +
+  scale_fill_viridis_c(guide = "none") +
+  labs(title = "Ke$ha - Cannibal",
+       x = "Time (s)", 
+       y = "Tempo (BPM)") +
+  theme_classic()
+
+subplot(ggplotly(cannibal_tempo),
+        ggplotly(cannibal_dtp))  
